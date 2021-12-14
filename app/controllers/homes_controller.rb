2b3b8a91a30931581bec6d7144c372b99248c0f1
vagrant <vagrant@localhost.localdomain>
2021-12-14 15:54:01 +0900

    add history

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -9,4 +9,7 @@ class HomesController < ApplicationController
   def about
   end
 
+  def history
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,7 @@ Rails.application.routes.draw do
   resources :bounties, only: [:index, :new, :edit, :create, :update]
 
   get 'about', to: 'homes#about'
+  get 'history', to: 'homes#history'
   root 'homes#top'
 
 end
